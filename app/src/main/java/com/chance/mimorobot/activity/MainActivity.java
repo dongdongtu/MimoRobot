70389ea2f66c2caff8dca31dd3b3d794a4d0d0a8
dongdongtu <duanledexianzzd@163.com>
2021-01-25 14:29:41 +0800

    1、日照中心

--- app/src/main/java/com/chance/mimorobot/activity/MainActivity.java
+++ app/src/main/java/com/chance/mimorobot/activity/MainActivity.java
@@ -153,7 +153,6 @@ public class MainActivity extends BaseActivity implements ActionRecycleViewAdapt
     private ActionRecycleViewAdapter actionRecycleViewAdapter;
     private RoomRecycleViewAdapter roomRecycleViewAdapter;
 
-
     private Dialog showAlertDialog;
 
 
